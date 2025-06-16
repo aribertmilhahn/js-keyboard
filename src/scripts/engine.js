9090df956550f1d8bd8b2c053131b64454c2d0b3
Aribert <aribertmilhahn@gmail.com>
2025-06-15 21:37:59 -0300

    adding distortion

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,13 +1,15 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 const volumeSlider = document.querySelector(".volume-slider input");
 const keysCheck = document.querySelector(".keys-check input");
+const distCheck = document.querySelector(".dist-check input");
 
 let mappedKeys = [];
+let isDistortionOn = false;
 
 let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
-    audio.src = `src/tunes/${key}.wav`;
+    audio.src = isDistortionOn ? `src/tunes/${key}-dist.wav` : `src/tunes/${key}.wav`;
     audio.play();
 
     const clickedKey = document.querySelector(`[data-key="${key}"]`);
@@ -16,8 +18,6 @@ const playTune = (key) => {
         clickedKey.classList.remove("active");
     }, 150);
 };
-
-
 
 pianoKeys.forEach((key) => {
     key.addEventListener("click", () => playTune(key.dataset.key));
@@ -38,5 +38,8 @@ const showHideKeys = () => {
     pianoKeys.forEach((key) => key.classList.toggle("hide"));
 };
 
+const toggleDistortion = () => isDistortionOn = !isDistortionOn;
+
 volumeSlider.addEventListener("input", handleVolume);
 keysCheck.addEventListener("click", showHideKeys);
+distCheck.addEventListener("click", toggleDistortion)
